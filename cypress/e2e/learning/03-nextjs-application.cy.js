6727aeca378cc2a31c6d5c636c74d26d1363eaaf
sarthaksavvy <sarthak@bitfumes.com>
2022-08-08 19:31:01 +0530

    09 Cypress course - intercept post api call

--- cypress/e2e/learning/03-nextjs-application.cy.js
+++ cypress/e2e/learning/03-nextjs-application.cy.js
@@ -18,4 +18,35 @@ describe("test feeds are available", () => {
       .should("be.visible")
       .and("have.length", 2);
   });
+
+  it("sign in the user", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get("input[type='email'").type("abc@gmail.com");
+    cy.get("input[type='password'").type("password");
+    cy.get("form").submit();
+  });
+
+  it("create new feed post", () => {
+    // assert that only 2 post are there
+    cy.intercept("get", "http://localhost:3000/api/posts?limit=10", {
+      fixture: "posts.json",
+    });
+
+    cy.get(".PostList_root__Cj_24")
+      .find(".Post_root__6WEkA")
+      .should("be.visible")
+      .and("have.length", 2);
+
+    cy.intercept("POST", "http://localhost:3000/api/posts", {
+      body: { content: "I am from cypress again" },
+    });
+
+    // create new post
+    cy.get(".Input_input__fo8G3")
+      .type("I am from cypress")
+      .then(() => {
+        cy.get("form").submit();
+        cy.contains("You have posted successfully");
+      });
+  });
 });
